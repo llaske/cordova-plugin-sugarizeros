d557ad3a2bcade58d74ab3920bd4e9510165a626
mikklfr <mohayon75@gmail.com>
2016-10-11 23:16:27 +0200

    Thread pool, caching tricks

--- android/SharedPreferencesManager.java
+++ android/SharedPreferencesManager.java
@@ -11,31 +11,34 @@ public class SharedPreferencesManager {
     final static String IS_SETUP_TAG = "IS_SETUP";
     final static String KEYSTORE_TAG = "KEYSTORE";
 
-    public static void putInt(Context context, String tag, int value){
+    public static void putInt(Context context, String tag, int value) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putInt(tag, value);
         editor.apply();
     }
-    public static void putString(Context context, String tag, String value){
+
+    public static void putString(Context context, String tag, String value) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString(tag, value);
         editor.apply();
     }
 
-    public static void getString(CallbackContext callbackContext, Context context, String tag){
+    public static void getString(CallbackContext callbackContext, Context context, String tag) {
         callbackContext.success(getString(context, tag));
     }
 
-    public static void getInt(CallbackContext callbackContext, Context context, String tag){
+    public static void getInt(CallbackContext callbackContext, Context context, String tag) {
         callbackContext.success(getInt(context, tag));
     }
-    public static int getInt(Context context, String tag){
+
+    public static int getInt(Context context, String tag) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getInt(tag, -1);
     }
-    public static String getString(Context context, String tag){
+
+    public static String getString(Context context, String tag) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(tag, "");
     }
