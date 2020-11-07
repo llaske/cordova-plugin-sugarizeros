87ede5aef4c7a8dffe2b9adb330ff0a0c2f7e1af
Lionel Laské <llaske@c2s.fr>
2020-11-07 10:13:38 +0100

    Fix autologoff issue when app name is not sugarizer

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-rm -f "platforms/android/src/org/olpcfrance/sugarizer/MainActivity.java"
-rm -f "platforms/android/src/org/olpc_france/sugarizer/MainActivity.java"
+CONFIGID=$(grep 'id[[:blank:]]*=[[:blank:]]*"org.olpc_france..*."' config.xml | sed -e "s/.*id[ \t]*=[ \t]*\"org.olpc_france.sugarizeros\([^\"]*\)\".*/\1/")
+rm -f platforms/android/src/org/olpc_france/sugarizer*/MainActivity.java
+if [ -n "$CONFIGID" ]; then
+  cp plugins/cordova-plugin-sugarizeros/android/MainActivity.java platforms/android/src/org/olpc_france/sugarizer$CONFIGID/
+fi
